87343540783d6376230380579bf19a37755497c3
Subashk1432 <727721euai055@skcet.ac.in>
2023-12-20 22:05:55 +0530

    first commit

---
+++ src/pages/claim.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../navbar'
+
+export default function Claim() {
+  return (
+    <>
+    <div>
+    <Navbar/>
+    </div>
+    <div>
+      this is claim page
+    </div>
+    </>
+  )
+}
